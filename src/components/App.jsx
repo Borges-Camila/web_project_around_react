a74a9a6e56e13da5eb8791af59c8101a4ad57575
Camila Borges <camis02121999@gmail.com>
2025-01-23 16:31:57 -0300

    Removi o const com o arrei de cartões da Main, e puxei os cart~es pela API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { useEffect } from "react";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
+import infoAPI from "./Utils/api";
 
 function App() {
+  const cards = [];
+
+  useEffect(() => {
+    infoAPI
+      .getInitialCards()
+      .then((res) => res.json())
+      .then((cards) => {
+        console.log(cards);
+      })
+      .catch((error) => {
+        console.group("Error get", error);
+      });
+  });
+
   return (
-    <>
-      <div className="page">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    </>
+    <div className="page">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Main/components/Card/Card.jsx
+++ src/components/Main/components/Card/Card.jsx
@@ -4,23 +4,19 @@ import heartButton from "../../../../images/Coração.svg";
 export default function Card() {
   const { name, link } = props.card;
   return (
-    <section className="elements">
-      <template id="card-template">
-        <div className="element">
-          <img
-            src={binButton}
-            alt="Imagem de uma lixeira branca."
-            className="element__trash"
-          />
+    <div className="element">
+      <img
+        src={binButton}
+        alt="Imagem de uma lixeira branca."
+        className="element__trash"
+      />
 
-          <img src={link} alt="" className="element__image" />
+      <img src={link} alt="" className="element__image" />
 
-          <div className="element__content">
-            <h2 className="element__place-name">{name}</h2>
-            <img src={heartButton} alt="Coração." className="element__heart" />
-          </div>
-        </div>
-      </template>
-    </section>
+      <div className="element__content">
+        <h2 className="element__place-name">{name}</h2>
+        <img src={heartButton} alt="Coração." className="element__heart" />
+      </div>
+    </div>
   );
 }
