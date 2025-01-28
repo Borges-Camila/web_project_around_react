dda8c97355ab2a5106c6eccf031065a81b1bebbf
Camila Borges <camis02121999@gmail.com>
2025-01-28 10:22:59 -0300

    Ajustes no erro apontado no arquivo 'eslint.config.js', API removida e mock data utilizada para mostrar os cartões na tela

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,12 @@
-import { useEffect, useState } from "react";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
-import infoAPI from "./Utils/api";
 
 function App() {
-  const [cards, setCards] = useState([]); // const dos cartões
-
-  useEffect(() => {
-    infoAPI
-      .getInitialCards()
-      .then((res) => res.json())
-      .then((cards) => {
-        setCards(cards);
-      })
-      .catch((error) => {
-        console.log("Error get", error);
-      });
-  });
-
-  function handleDeleteCard(card) {
-    infoAPI
-      .delete(card._id)
-      .then((res) => {
-        if (res.status !== 200) {
-          return Promise.reject("Erro no delete card");
-        }
-        setCards(cards.filter((c) => c._id !== card._id));
-      })
-      .catch((error) => {
-        console.error(`[DELETE] - cards - ${error}`);
-      });
-  }
-
   return (
     <div className="page">
       <Header />
-      <Main cards={cards} handleDeleteCard={handleDeleteCard} />
+      <Main />
 
       <Footer />
     </div>
--- src/components/Main/components/Card/Card.jsx
+++ src/components/Main/components/Card/Card.jsx
@@ -3,36 +3,28 @@ import heartButton from "../../../../images/Coração.svg";
 
 import ImagePopup from "../Popup/components/BigImage/imagePopup";
 
-export default function Card({ card, handleDeleteCard, handleOpenPopup }) {
-  const imageComponent = {
-    title: "",
-    children: <ImagePopup card={card} />,
-  };
-
-  function deleteCard() {
-    handleDeleteCard(card);
-  }
+export default function Card(props) {
+  const { name, link, isLiked } = props.card;
 
   return (
-    <div className="element">
+    <li className="element">
       <img
         src={binButton}
         alt="Imagem de uma lixeira branca."
         className="element__trash"
-        onClick={deleteCard}
       />
 
       <img
-        src={card.link}
-        alt=""
+        src={link}
+        alt={name}
         className="element__image"
         onClick={() => handleOpenPopup(imageComponent)}
       />
 
       <div className="element__content">
-        <h2 className="element__place-name">{card.name}</h2>
+        <h2 className="element__place-name">{name}</h2>
         <img src={heartButton} alt="Coração." className="element__heart" />
       </div>
-    </div>
+    </li>
   );
 }
