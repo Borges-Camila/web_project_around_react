37084c8fd2767f376a5dd66402a96c2deb0483dd
Camila Borges <camis02121999@gmail.com>
2025-02-04 16:06:02 -0300

    Na main: adicionada uma variável de estado de cards com um array vazio como valor padrão, excluídos os mock data, importado o api. Adicionado o hook useEffect para fazer a solicitação a API para buscar os dados dos cartões

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,31 +2,13 @@ import profileAvatar from "../../images/Avatar.png";
 import profileEditButton from "../../images/Edit-Button.png";
 import profileAddButton from "../../images/Add-Button.png";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewCard from "./components/Popup/components/NewCard/NewCard";
 import Popup from "./components/Popup/Popup";
 import EditProfile from "./components/Popup/components/EditProfile/EditProfile";
 import EditAvatar from "./components/Popup/components/EditAvatar/EditAvatar";
 import Card from "./components/Card/Card";
-
-const cards = [
-  {
-    isLiked: false,
-    _id: "5d1f0611d321eb4bdcd707dd",
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-    owner: "5d1f0611d321eb4bdcd707dd",
-    createdAt: "2019-07-05T08:10:57.741Z",
-  },
-  {
-    isLiked: false,
-    _id: "5d1f064ed321eb4bdcd707de",
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-    owner: "5d1f0611d321eb4bdcd707dd",
-    createdAt: "2019-07-05T08:11:58.324Z",
-  },
-];
+import api from "../../utils/api";
 
 function Main() {
   const [popup, setPopup] = useState(null);
@@ -50,6 +32,19 @@ function Main() {
   function handleClosePopup() {
     setPopup(null);
   }
+  const [cards, setCards] = useState([]); //variável de estado para cards
+
+  useEffect(() => {
+    api
+      .getInitialCards()
+      .then((res) => res.json())
+      .then((cards) => {
+        setCards(cards);
+      })
+      .catch((error) => {
+        console.log("Error get", error);
+      });
+  });
 
   return (
     <>
